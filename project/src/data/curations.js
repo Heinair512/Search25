61fd6ce96b5a393fd27c7276eaa995efcd84c7a7
Heinair512 <h.holzhueter@mailbox.org>
2025-05-16 12:08:43 +0200

    Delete project directory

--- project/src/data/curations.js
+++
@@ -1,9 +0,0 @@
-import { ref } from 'vue';
-
-export const curations = ref([
-  { term: 'hammer', pinnedCount: 3, pinnedKbns: ['123456', '789012'] },
-  { term: 'schrauben', pinnedCount: 2, pinnedKbns: ['345678'] },
-  { term: 'bohrer', pinnedCount: 1 },
-  { term: 'säge', pinnedCount: 2 },
-  { term: 'werkzeugkasten', pinnedCount: 1 }
-]);
