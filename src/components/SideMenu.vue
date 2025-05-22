dbc72555f5dcdee0cda7b001c48938e9a3860ea3
Heinair512 <h.holzhueter@mailbox.org>
2025-05-22 16:59:56 +0200

    Add required Vue component structure

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,1 +1,9 @@
-<command>Content of SideMenu.vue with updated translation key usage</command>
+<template>
+  <div class="side-menu">
+    <!-- Content will go here -->
+  </div>
+</template>
+
+<script setup>
+// Component logic will go here
+</script>
