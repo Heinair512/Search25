54b1af539cd563f1c6e5ce511f9783f8cdc34967
Heinair512 <h.holzhueter@mailbox.org>
2025-05-22 17:21:17 +0200

    Add required template and script blocks to Profile component

--- src/screens/Profile.vue
+++ src/screens/Profile.vue
@@ -1,1 +1,15 @@
-<command>cat src/screens/Profile.vue</command>
+<template>
+  <div class="profile">
+    <h1>Profile</h1>
+  </div>
+</template>
+
+<script setup>
+// Component logic will go here
+</script>
+
+<style scoped>
+.profile {
+  padding: 1rem;
+}
+</style>
