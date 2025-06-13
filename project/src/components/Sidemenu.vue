6b1bf032c5d292b3323a0f292a9cdea067fb0dec
Heinair512 <h.holzhueter@mailbox.org>
2025-06-13 11:49:28 +0200

    Add files via upload

---
+++ project/src/components/Sidemenu.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <Sidebar>
+    <Menu>
+      <MenuItem label="Dashboard" icon="pi pi-home" to="/" />
+      <MenuItem label="Search Preview" icon="pi pi-search" to="/search-preview" />
+      <MenuItem label="Technical" icon="pi pi-cog">
+        <MenuItem label="Debug-Suche" icon="pi pi-search" to="/technical/debug-search" v-if="hasAccess(['Admin', 'DEV'])" />
+      </MenuItem>
+    </Menu>
+  </Sidebar>
+</template>
+
+<script setup>
+import { useAuthStore } from '@/store/auth';
+const auth = useAuthStore();
+
+function hasAccess(roles) {
+  return roles.includes(auth.role);
+}
+</script>
